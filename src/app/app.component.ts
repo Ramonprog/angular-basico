71bb4b16f6cfad00f9cd2f319c011a9bbc656a5e
Ramonprog <programador.ramon@gmail.com>
2024-05-11 18:34:15 -0300

    learn angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Componente01Component } from './componente01/componente01.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Componente01Component],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-basico';
